6d00edb464cafa86d88a49a4e3b72f4e2d9b2151
christopher Dikesa <chrismsmr@gmail.com>
2026-08-30 14:51:31 +0200

    Add Prettier configuration file

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'only-multiline',
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  proseWrap: 'preserve',
+};
