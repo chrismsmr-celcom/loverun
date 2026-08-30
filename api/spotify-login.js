1ef04049285fd6fb958d1bdbdfffd7523c6a30bf
christopher Dikesa <chrismsmr@gmail.com>
2026-08-30 13:42:40 +0200

    Make redirect_uri dynamic and update scope format
    Updated redirect_uri to be dynamic based on request headers and changed scope to an array format.

--- api/spotify-login.js
+++ api/spotify-login.js
@@ -1,9 +1,18 @@
 export default function handler(req, res) {
   const client_id = process.env.SPOTIFY_CLIENT_ID;
-  const redirect_uri = 'https://loverun-silk.vercel.app/'; // Ton URL Vercel
-  const scope = 'streaming user-read-email user-read-private user-modify-playback-state';
+  const protocol = req.headers['x-forwarded-proto'] || 'https';
+  const host = req.headers.host;
+  const redirect_uri = `${protocol}://${host}/`;
+
+  const scope = [
+    'streaming',
+    'user-read-email',
+    'user-read-private',
+    'user-modify-playback-state',
+    'user-read-playback-state'
+  ].join(' ');
 
   const authUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${client_id}&scope=${encodeURIComponent(scope)}&redirect_uri=${encodeURIComponent(redirect_uri)}`;
-  
+
   res.redirect(authUrl);
 }
